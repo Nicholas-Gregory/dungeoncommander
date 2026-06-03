bf0c8698a79e0a21ba4f63b4ae08e1a35a2703d3
Nicholas Gregory <nicholas.babcock092@gmail.com>
2026-06-03 13:26:39 -0700

    (IO flow skeleton)

--- app/client.hs
+++ app/client.hs
@@ -5,12 +5,21 @@ import DC.Opts (command)
 import Network.Socket
 import qualified Data.ByteString.Char8 as C
 import Network.Socket.ByteString (sendAll)
+import Control.Applicative (optional)
+import System.IO (hReady, stdin)
 
 main :: IO ()
 main = withSocketsDo $ do
-  (cmd:_) <- getArgs
+  args <- getArgs
   sock <- socket AF_UNIX Stream defaultProtocol
+  ready <- hReady stdin
 
   connect sock (SockAddrUnix "/tmp/dc.sock")
 
-  sendAll sock (C.pack cmd)
+  if ready
+    then do
+      input <- getContents
+      putStrLn ("Working with: " ++ input)
+    else do
+      putStrLn "Requesting data from server"
+      sendAll sock (C.pack "REQUEST")
